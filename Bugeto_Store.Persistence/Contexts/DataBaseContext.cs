9959830923314d8819ac5a39cd4899ac1e9c1dc8
alishajerat <shajerat13037@gmail.com>
2021-06-08 11:41:44 +0430

    Create Entities and DataBaseContext

---
+++ Bugeto_Store.Persistence/Contexts/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Bugeto_Store.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugeto_Store.Persistence.Contexts
+{
+   public class DataBaseContext:DbContext
+    {
+        public DataBaseContext(DbContextOptions options) :base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserInRole> Userin { get; set; }
+
+    }
+}
